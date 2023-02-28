49b1867b268be79390a4a51a4a53face5f33feb5
Nicolas Acosta <niicooacosta@gmail.com>
2023-02-28 00:08:29 -0300

    primer escritura

---
+++ main.js
@@ -0,0 +1,42 @@
+const names = document.getElementById("nombre");
+const name = document.getElementById("name");
+const apellido = document.getElementById("apellido")
+const foto = document.getElementById("foto")
+const phone = document.getElementById("phone")
+const email = document.getElementById("email")
+const generarPersona = async() => {
+    const url = 'https://randomuser.me/api/';
+    const respuesta = await fetch(url);
+    const { results } = await respuesta.json();
+    const datos = results[0];
+    console.log(datos);
+    foto.src = datos.picture.large;
+    names.textContent = datos.name.first + " "+ datos.name.last ;
+    name.textContent = datos.name.first + " "+ datos.name.last ;
+    phone.textContent = datos.phone ;
+    email.textContent = datos.email ;
+}
+function responsiveMenu() {
+    var x = document.getElementById("nav");
+    if (x.className === "") {
+        x.className = "responsive";
+    } else {
+        x.className = "";
+    }
+}
+function seleccionar(link) {
+    var opciones = document.querySelectorAll('#links  a');
+    opciones[0].className = "";
+    opciones[1].className = "";
+    opciones[2].className = "";
+    opciones[3].className = "";
+    opciones[4].className = "";
+    link.className = "seleccionado";
+}
+
+      generarPersona();
+ 
+      
+
+    
+    
